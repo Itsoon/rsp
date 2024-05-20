c3c07850d400cf4ffd632e9d32776602d7ad424c
Itsoon-xyz <91259186+Itsoon@users.noreply.github.com>
2024-05-20 16:51:18 +0200

    add user entry

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,66 @@
-use serde::Deserialize;
+use indexmap::IndexMap;
+use serde_json::Value;
 use std::fs;
+use std::io::{stdin, stdout, Write};
 
-mod persistent_cmd;
-
-use persistent_cmd::persistent_cmd;
-
-#[derive(Debug, Deserialize)]
-struct Profile {
-    run: Option<Vec<String>>,
-    #[serde(rename = "kitty-session")]
-    kitty_session: Option<Vec<String>>,
-    commands: Option<Vec<String>>,
-}
+mod launch_profile;
+use launch_profile::launch_profile;
 
 fn main() {
     let path = "./src/profile.json";
     let data = fs::read_to_string(path).expect("Unable to read file");
     let profiles: Vec<serde_json::Value> = serde_json::from_str(&data).expect("Unable to parse");
 
-    let profile = &profiles[0];
-    if let Some(profile_obj) = profile.as_object() {
-        if let Some((_profile_key, profile_value)) = profile_obj.iter().next() {
-            let profile_data: Profile =
-                serde_json::from_value(profile_value.clone()).expect("Unable to parse profile");
+    let mut available_profile = IndexMap::new();
 
-            if let Some(run) = &profile_data.run {
-                println!("{:?}", run);
-                for app in run {
-                    println!("{}", app);
-                    persistent_cmd(app, None);
-                }
-            }
-
-            if let Some(kitty_session) = &profile_data.kitty_session {
-                for command in kitty_session {
-                    let mut args = vec!["--session"];
-                    args.push(command);
-                    println!("{:?}", args);
-                    persistent_cmd("kitty", Some(&args));
-                }
-            }
-
-            if let Some(commands) = &profile_data.commands {
-                for command in commands {
-                    let mut args = vec!["sh", "-c"];
-                    let formatted_command = format!("{} && exec zsh", command);
-                    args.push(&formatted_command);
-                    persistent_cmd("kitty", Some(&args));
-                }
+    for (index, profile) in profiles.iter().enumerate() {
+        if let Value::Object(map) = profile {
+            for (key, _value) in map {
+                available_profile.insert(index as i32, key.to_string());
             }
         }
+    }
+
+    println!("Available profile :");
+
+    for (index, value) in available_profile.iter() {
+        println!("{}  {}", index, value);
+    }
+
+    println!();
+
+    print!("Enter a profile(s) : ");
+
+    let mut input = String::new();
+
+    let _ = stdout().flush();
+    stdin()
+        .read_line(&mut input)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    let input = input.trim();
+
+    if let Ok(number) = input.parse::<i32>() {
+        if available_profile.contains_key(&number) {
+            if let Some(user_value) = available_profile.get(&number) {
+                for value in profiles.iter() {
+                    if let Some(value) = value.get(user_value) {
+                        if let Ok(json_value) = serde_json::to_value(value) {
+                            launch_profile(&json_value);
+                        }
+                    }
+                }
+            }
+        } else {
+            println!("Bad character");
+        }
+    } else {
+        println!("Invalid input or failed parser");
     }
 }
