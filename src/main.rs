c5ea6388c2639b79ba104db812dad8c4e0d0db77
Itsoon-xyz <91259186+Itsoon@users.noreply.github.com>
2024-05-20 19:52:47 +0200

    add arguments
    use std::env

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use indexmap::IndexMap;
 use serde_json::Value;
+use std::env;
 use std::fs;
-use std::io::{stdin, stdout, Write};
+
+use std::process::exit;
 
 mod launch_profile;
 use launch_profile::launch_profile;
 
 fn main() {
-    let path = "./src/profile.json";
+    let args: Vec<String> = env::args().collect();
+
+    let arg: String;
+
+    if args.len() == 2 {
+        arg = args[1].clone();
+    } else {
+        display_help();
+        exit(0);
+    };
+
+    let home_dir = match std::env::var("HOME") {
+        Ok(val) => val,
+        Err(_) => panic!("HOME environment variable is not set"),
+    };
+
+    let path = format!("{}/.config/rsp/profile.json", home_dir);
     let data = fs::read_to_string(path).expect("Unable to read file");
     let profiles: Vec<serde_json::Value> = serde_json::from_str(&data).expect("Unable to parse");
 
@@ -21,34 +39,15 @@ fn main() {
         }
     }
 
-    println!("Available profile :");
-
-    for (index, value) in available_profile.iter() {
-        println!("{}  {}", index, value);
-    }
-
-    println!();
-
-    print!("Enter a profile(s) : ");
-
-    let mut input = String::new();
-
-    let _ = stdout().flush();
-    stdin()
-        .read_line(&mut input)
-        .expect("Did not enter a correct string");
-    if let Some('\n') = input.chars().next_back() {
-        input.pop();
-    }
-    if let Some('\r') = input.chars().next_back() {
-        input.pop();
-    }
-
-    let input = input.trim();
-
-    if let Ok(number) = input.parse::<i32>() {
-        if available_profile.contains_key(&number) {
-            if let Some(user_value) = available_profile.get(&number) {
+    if arg == "l" || arg == "ls" || arg == "list" {
+        list_available_profile(available_profile);
+        exit(0);
+    } else if arg == "-h" || arg == "-help" || arg == "--help" || arg == "help" {
+        display_help();
+        exit(0);
+    } else if let Ok(arg) = arg.parse::<i32>() {
+        if available_profile.contains_key(&arg) {
+            if let Some(user_value) = available_profile.get(&arg) {
                 for value in profiles.iter() {
                     if let Some(value) = value.get(user_value) {
                         if let Ok(json_value) = serde_json::to_value(value) {
@@ -59,8 +58,33 @@ fn main() {
             }
         } else {
             println!("Bad character");
+            display_help();
+            exit(0);
         }
     } else {
         println!("Invalid input or failed parser");
+        display_help();
+        exit(0);
     }
+}
+
+fn list_available_profile(available_profile: IndexMap<i32, String>) {
+    for (index, value) in available_profile.iter() {
+        println!("{}  {}", index, value);
+    }
+}
+
+fn display_help() {
+    let help: &str = "
+Usage: starter-profile [COMMAND]
+
+Commands:
+  l, ls, list   Print available profiles
+  <profile>  Processing profile
+  help       Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version";
+    println!("{}", help);
 }
